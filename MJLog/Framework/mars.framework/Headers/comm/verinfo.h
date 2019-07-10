b5bc56d9986bf8b26da53f0f086b7f8c717544bd
xiadong <dong.xia@musjoy.com>
2019-07-10 14:27:19 +0800

    support i386

--- MJLog/Framework/mars.framework/Headers/comm/verinfo.h
+++ MJLog/Framework/mars.framework/Headers/comm/verinfo.h
@@ -5,7 +5,7 @@
 #define MARS_REVISION ""
 #define MARS_PATH ""
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-05 15:17:12"
+#define MARS_BUILD_TIME "2019-07-10 14:19:01"
 #define MARS_TAG ""
 
 #endif
